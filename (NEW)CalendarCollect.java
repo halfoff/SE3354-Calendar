a991d55443a849afa6996c5ca489a8f5285ec70c
LeeBalser <leejbalser@gmail.com>
2015-11-09 21:03:49 -0600

    Create (NEW)CalendarCollect.java

---
+++ (NEW)CalendarCollect.java
@@ -0,0 +1,15 @@
+package com.example.lee.calendar;
+
+import java.util.ArrayList;
+
+public class CalendarCollect
+{
+    public String date = "";
+    public String event_message = "";
+    public static ArrayList<CalendarCollect> date_collection_arr;
+    public CalendarCollect(String date,String event_message)
+    {
+        this.date = date;
+        this.event_message = event_message;
+    }
+}
