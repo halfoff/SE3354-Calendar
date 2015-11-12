9a9fb623e455d9f79d90f31c47d22c08d7f25dc2
Matthew <mac130130@utdallas.edu>
2015-11-11 18:22:23 -0600

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -26,7 +26,7 @@ public class Calendar extends Activity
 
         tv_month = (TextView) findViewById(R.id.tv_month);
         calendar_month = (GregorianCalendar) GregorianCalendar.getInstance();
-        tv_month.setText(android.text.format.DateFormat.format("mmmm yyyy", calendar_month));
+        tv_month.setText(android.text.format.DateFormat.format("MM yyyy", calendar_month));
 
         calendar_month_clone = (GregorianCalendar) calendar_month.clone();
         calendar_adapter = new CalendarAdapter(this, calendar_month,CalendarCollect.date_collection_arr);
